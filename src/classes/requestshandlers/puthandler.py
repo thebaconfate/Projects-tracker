365190b1a82448e77ab1ea2b0ca8bd58ce610dc5
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-08-26 02:45:19 +0200

    added more skeleton code for put methods

--- src/classes/requestshandlers/puthandler.py
+++ src/classes/requestshandlers/puthandler.py
@@ -1,8 +1,38 @@
+from src.classes.customerrors.inputerror import InputException
 
 
 ''' class to handle put requests'''
+
+
 class Puthandler():
 
     def __init__(self, db):
         self.db = db
-                 
+
+    def switch_stage(self, key, value):
+        # TODO implement switch stage
+        match key:
+            case 'name':
+                pass
+            case 'price':
+                pass
+            case 'time':
+                pass
+            case _:
+                raise InputException('invalid payload to update stage')
+
+    def update_stage(self, project_id, stage_id, payload):
+        # TODO implement this
+        pass
+    
+    def switch_project(self, key, value):
+        # TODO implement switch project
+        match key:
+            case 'name':
+                pass
+            case _:
+                raise InputException('invalid payload to update project')
+            
+    def update_project(self, project_id, payload):
+        # TODO implement this
+        pass
