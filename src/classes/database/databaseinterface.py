060f48207e8ac99351c92b836197b5b0b6049cc1
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-09-02 17:25:57 +0200

    added todo information

--- src/classes/database/databaseinterface.py
+++ src/classes/database/databaseinterface.py
@@ -2,7 +2,7 @@ import os
 import mysql.connector
 
 
-# TODO make the queries return dictionaries instead of tuples so it can be unpacked using **kwargs
+# TODO make the queries return dictionaries instead of tuples so it can be unpacked using **kwargs more found at: https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-cursor.html
 class DatabaseInterface:
     def __enter__(self):
         self.mysql = mysql.connector.connect(
