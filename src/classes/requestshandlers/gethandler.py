e0ed010b4da122ee39e5a5b3589c6b828b62b756
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-09-13 20:34:46 +0200

    refactored calc

--- src/classes/requestshandlers/gethandler.py
+++ src/classes/requestshandlers/gethandler.py
@@ -61,12 +61,9 @@ class GetHandler:
             retrieved_stages = db.get_time_and_price(project_id, user.id)
         result = 0
         for stage in retrieved_stages:
-            days = stage[0]
-            seconds = stage[1]
-            price = stage[2]
-            total_hours = days * 24 + seconds // 3600
-            print(total_hours)
-            total_mins = (seconds % 3600) // 60
-            print(total_mins)
-            result += total_hours * price + (total_mins // 15) * (price / 4)
+            price_per_hr = stage["price"]
+            if price_per_hr == 0:
+                continue
+            result += price_per_hr * stage["days"] * 24
+            result += stage["seconds"] // 60 // 15 * price_per_hr / 4
         return {"total value": result}
