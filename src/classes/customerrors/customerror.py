ec0ac1ee90db7bdb345f3a1f90e107cc92dc948a
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-03-25 22:45:09 +0100

    restarted project

--- src/classes/customerrors/customerror.py
+++
@@ -1,7 +0,0 @@
-class CustomError(Exception):
-    def __init__(self, error_message):
-        super().__init__(error_message)
-        self.error_message = error_message
-
-    def __str__(self):
-        return self.error_message
--- src/setup.py
+++
@@ -1,7 +0,0 @@
-from pytz import timezone
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-
-login_manager = LoginManager()
-tz = timezone("Europe/Brussels")
-bcrypt = Bcrypt()
