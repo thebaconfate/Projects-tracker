b1ce24c3a4663999715601e7a250baa9c7e1f99a
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-03-28 17:12:09 +0100

    added connection mock in every test case

--- tests/test_classes/database/test_interface.py
+++ tests/test_classes/database/test_interface.py
@@ -1,3 +1,4 @@
+import mock
 import pytest
 from unittest.mock import patch, AsyncMock
 from src.classes.database.interface import DatabaseInterface
@@ -5,9 +6,12 @@ from src.classes.database.interface import DatabaseInterface
 
 class TestDatabaseInterface:
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def mysql_patch(self):
-        yield patch("mysql.connector.aio.connect", new_callable=AsyncMock)
+        with patch(
+            "mysql.connector.aio.connect", new_callable=AsyncMock
+        ) as mock_connect:
+            yield mock_connect
 
     @pytest.fixture
     def database_args(self):
@@ -24,37 +28,31 @@ class TestDatabaseInterface:
         yield "src.classes.database.interface.DatabaseInterface"
 
     @pytest.mark.asyncio
-    async def test_aenter_called(
-        self, database_interface_patch, database_args, mysql_patch
-    ):
-        with mysql_patch:
-            with patch(
-                database_interface_patch + ".__aenter__", new_callable=AsyncMock
-            ) as mock_aenter:
-                async with DatabaseInterface(**database_args):
-                    pass
-                assert mock_aenter.called
+    async def test_aenter_called(self, database_interface_patch, database_args):
+        with patch(
+            database_interface_patch + ".__aenter__", new_callable=AsyncMock
+        ) as mock_aenter:
+            async with DatabaseInterface(**database_args):
+                pass
+            assert mock_aenter.called
 
     @pytest.mark.asyncio
-    async def test_aexit_called(
-        self, database_interface_patch, mysql_patch, database_args
-    ):
-        with mysql_patch:
-            with patch(
-                database_interface_patch + ".__aexit__", new_callable=AsyncMock
-            ) as mock_aexit:
-                async with DatabaseInterface(**database_args):
-                    pass
-                assert mock_aexit.called
+    async def test_aexit_called(self, database_interface_patch, database_args):
+        with patch(
+            database_interface_patch + ".__aexit__", new_callable=AsyncMock
+        ) as mock_aexit:
+            async with DatabaseInterface(**database_args):
+                pass
+            assert mock_aexit.called
 
     @pytest.mark.asyncio
     async def test_database_interface(self, mysql_patch, database_args):
         """Test that the database interface can be created and connected to a database,\
             this tests the following methods: __init__, __connect, __aenter__, __close, __aexit__"""
-        with mysql_patch as mock_connect:
-            mock_connection = mock_connect.return_value
-            async with DatabaseInterface(**database_args) as db_interface:
-                # Test that connection is established
-                assert mock_connect.called
-            # Test that connection is closed after exiting the with block
-            mock_connection.close.assert_called_once()
+        mock_connect = mysql_patch
+        mock_connection = mock_connect.return_value
+        async with DatabaseInterface(**database_args) as db_interface:
+            # Test that connection is established
+            assert mock_connect.called
+        # Test that connection is closed after exiting the with block
+        mock_connection.close.assert_called_once()
