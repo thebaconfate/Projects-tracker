92f5bd5d6d700ac1fe391a36dd59299158e731fb
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-03-28 18:46:36 +0100

    added custom errors

---
+++ src/classes/errors/database.py
@@ -0,0 +1,28 @@
+class DatabaseError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatabaseConnectionError(DatabaseError):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatabaseCursorError(DatabaseError):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatabaseUserError(DatabaseError):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatabaseUserExistsError(DatabaseUserError):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
