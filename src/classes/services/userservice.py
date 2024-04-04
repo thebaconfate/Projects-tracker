f4af897ea005c1756a77a3515400ba08a613caca
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-04-05 01:29:16 +0200

    Add UserService and update login and register routes

---
+++ src/classes/services/userservice.py
@@ -0,0 +1,48 @@
+import logging
+from fastapi import HTTPException, status
+from src.classes.services.authservice import AuthService
+from src.classes.errors.database import DatabaseUserAlreadyExistsError
+from src.classes.database.interface import DatabaseInterface
+from src.classes.models.user import NewUserModel
+
+
+class UserService:
+    def __init__(self, user: NewUserModel):
+        """This class does not expect any arguments to be passed to it. It is used to interact with the database to perform operations on the user table."""
+        self.user: NewUserModel = user
+
+    async def __aenter__(self):
+        """Entering the context manager sets the email and username to lowercase"""
+        await self.__lower()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        """Exiting the context manager does not have to do anything."""
+        pass
+
+    async def __lower(self) -> None:
+        if self.user.email:
+            self.user.email = self.user.email.lower()
+        if self.user.username:
+            self.user.username = self.user.username.lower()
+
+    async def __hash_password(self, auth: AuthService = AuthService()):
+        self.user.password = await auth.hash_password(self.user.password)
+
+    async def add_user(self) -> None:
+        """Add a user to the database"""
+        try: 
+            await self.__hash_password()
+            async with DatabaseInterface() as db:
+                return await db.save_user(**self.user.model_dump())
+        except (DatabaseUserAlreadyExistsError, RuntimeError):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=str(DatabaseUserAlreadyExistsError().message)
+            )
+        except Exception as e:
+            logging.error(f"Error adding user: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
+            )
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,16 +1,13 @@
 import logging
 import os
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Response, status
 from fastapi.responses import JSONResponse, RedirectResponse
 
-from src.classes.errors.database import DatabaseConnectionError, DatabaseUserAlreadyExistsError
+from src.classes.services.userservice import UserService
 from src.classes.models.auth import Token
 from src.classes.services.authservice import AuthService
-from src.classes.models.user import UserModel
+from src.classes.models.user import LoginUserModel, NewUserModel
 from src.classes.database.interface import DatabaseInterface
-
-router_prefix = "/users"
-
 
 async def get_secret_key() -> str:
     return os.getenv(key="SECRET_KEY")
@@ -25,25 +22,30 @@ async def get_token_expiration() -> int:
 
 
 router = APIRouter(
-    prefix=router_prefix,
+    prefix="/users",
     tags=["users"],
     responses={404: {"description": "Not found"}},
 )
 
 
 @router.post(path=("/login"))
-async def login(user: UserModel):
+async def login(user: LoginUserModel):
     """Logs a user in by checking the database for a matching username and password"""
     error_detail = "Incorrect username or password"
     error_header = {"WWW-Authenticate": "Bearer"}
+    print(user)
     try:
         async with DatabaseInterface() as db:
             if user.email is not None:
                 user.email = user.email.lower()
+                print("checking email")
                 result = await db.get_user_by_email(email=user.email)
+                print("got result")
             elif user.username is not None:
                 user.username = user.username.lower()
+                print("checking username")
                 result = await db.get_user_by_username(username=user.username.lower())
+                print("got result")
             else:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -60,13 +62,17 @@ async def login(user: UserModel):
         )
     else:
         auth = AuthService()
+        print("authenticating user")
         auth_user = await auth.authenticate_user(user=user, user_in_db=result)
+        print("got auth user")
         if auth_user:
+            print("generating token")
             token = Token(
                 access_token=await auth.generate_jwt_token(auth_user),
                 token_type="bearer",
             )
             response = JSONResponse(content="Authentication successful")
+            print("setting cookie")
             response.set_cookie(key=token.token_type, value=token.access_token)
             return response
         else:
@@ -78,38 +84,19 @@ async def login(user: UserModel):
 
 
 @router.post(path=("/register"))
-async def register(user: UserModel):
-    """Registers a new user by adding them to the database"""
-    if user.email and user.username:
-        try:
-            async with DatabaseInterface() as db:
-                auth = AuthService()
-                user.email = user.email.lower()
-                user.username = user.username.lower()
-                user.password = await auth.hash_password(user.password)
-                await db.save_user(**user.model_dump())
-        except (DatabaseUserAlreadyExistsError, RuntimeError):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=str(DatabaseUserAlreadyExistsError().message),
-            )
-        except DatabaseConnectionError as e:
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=str(e),
-            )
-        except Exception as e:
-            logging.ERROR(msg=f"An unknown error occurred: {e}")
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="an unknown error occurred",
-            )
-        else:
-            return RedirectResponse(
-                url=f"{router_prefix}/login",
-            )
-    else:
+async def register(user: NewUserModel):
+    """Registers a new user by adding them to the database, this requires a unique email address, a username and a password."""
+    try:
+        async with UserService(user) as user_service:
+            await user_service.add_user()
+    except HTTPException as e:
+        raise e
+    except Exception as e:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username and email required",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
         )
+    else:
+        response = Response(status_code=status.HTTP_201_CREATED, content="User registered successfully")    
+        response.set_cookie(key="username", value=user.username)
+        return response
