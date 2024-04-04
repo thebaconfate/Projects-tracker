c3fdc4cb70ec2a49de0536473727aecdbff21653
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-04-05 01:28:50 +0200

    Refactor user models and add validation

--- src/classes/models/user.py
+++ src/classes/models/user.py
@@ -1,11 +1,33 @@
-from pydantic import BaseModel
+from typing import Any, Optional
+from pydantic import BaseModel, EmailStr, model_validator
 
-
-class UserModel(BaseModel):
-    email: str | None = None
-    username: str | None = None
+class BaseUserModel(BaseModel):
+    email: EmailStr
+    username: str
     password: str
 
+    @model_validator(mode='after')
+    def lower(self) -> 'BaseUserModel':
+        if self.email:
+            self.email = self.email.lower()
+        if self.username:
+            self.username = self.username.lower()
+        return self
 
-class UserDBModel(UserModel):
+class NewUserModel(BaseUserModel):
+    pass
+
+class LoginUserModel(BaseUserModel):
+    email: Optional[EmailStr]
+    username: Optional[str]
+
+    @model_validator(mode='before')
+    @classmethod
+    def check_existance_email_or_username(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            if not data.get('email') and not data.get('username'):
+                raise ValueError("Either email or username must be provided")
+        return data
+
+class DBUserModel(BaseUserModel):
     id: int
