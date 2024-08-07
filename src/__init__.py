686a90a47c74ccb4a90c58e612e484df14a020c1
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-08-07 10:32:47 -0400

    bugfix

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+from src.database import HOST, PORT
 from src.database.databaseinitiator import DatabaseInitiator
 
 load_dotenv(override=True)
 
 TESTING = os.getenv("TESTING")
 if not TESTING:
+    print(PORT, HOST)
     with DatabaseInitiator() as db:
         db.init_db()
